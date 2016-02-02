c03980ef6aac60a3938ab5ff3ca639c84a8750ca
Henry Kucab <kucab2345@gmail.com>
2016-02-02 14:25:06 -0700

    Added in basic test cases and a TestCase project to the solution for Dependency Graphs

---
+++ Spreadsheet/DependencyGraphTester/DependencyGraphTesting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Dependencies;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace DependencyGraphTester
+{
+    [TestClass]
+    public class DependencyGraphTesting
+    {
+        [TestMethod]
+        public void GraphTestMethod1()
+        {
+            DependencyGraph d = new DependencyGraph();
+        }
+        [TestMethod]
+        public void GraphTestMethod2()
+        {
+            DependencyGraph d = new DependencyGraph();
+            Debug.Assert(d.Size == 0);
+        }
+    }
+}
