31902df0c402fd6b809996ff5b2314b4e9979a52
Johnny Cash <kucab2345@gmail.com>
2016-03-05 11:53:48 -0700

    Red X closing dialogue in event of not saving

--- Spreadsheet/SpreadsheetGUI/SpreadsheetGUI.cs
+++ Spreadsheet/SpreadsheetGUI/SpreadsheetGUI.cs
@@ -226,5 +226,21 @@ namespace SpreadsheetGUI
                 Message = error.Message;
             }
         }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+
+            // Confirm user wants to close
+            switch (MessageBox.Show(this, "Are you sure you want to close?\nYou will lose any unsaved changes.", "Closing", MessageBoxButtons.YesNo))
+            {
+                case DialogResult.No:
+                    e.Cancel = true;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
